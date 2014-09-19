32c66307059cc7539355e04f063cab20fdbe01ef
davide <davide@DB.fastwebnet.it>
2014-09-20 00:42:33 +0200

    Aggiunta dei form per il ciclo di analisi e la gestione delle sessioni

--- src/GUI/spia/CicloDiAnalisi.java
+++ src/GUI/spia/CicloDiAnalisi.java
@@ -100,7 +100,11 @@ public class CicloDiAnalisi extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+                    new Thread( new Runnable() {
+              public void run() {
+                new SostituzioneLettera().setVisible(true);
+              }
+            }).start();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
